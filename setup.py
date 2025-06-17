ded2175477a53f14a64987358048573b722096c5
a01659147@tec.mx <a01659147@tec.mx>
2025-06-17 01:35:16 -0600

    noob error

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
@@ -9,7 +9,8 @@ setup(
     packages=find_packages(),
     install_requires=[],
     author="alan1x",
-    description=long_description,
+    description="Ready to go implementation for sending emails with python, a simple wrapper for stmp",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alan1x/ezwmails.git",
 )
